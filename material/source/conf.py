3d42fd3eb4340d9cf4fb3dc6cd8ce17726ae7fd4
Tiago A. Martins <tiagoammartins@gmail.com>
2017-07-01 04:51:16 -0300

    Adiciona extensão doctest no Sphinx

--- material/source/conf.py
+++ material/source/conf.py
@@ -31,8 +31,11 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.todo',
-    'sphinx.ext.mathjax']
+extensions = [
+    'sphinx.ext.todo',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.doctest'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
