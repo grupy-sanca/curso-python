a8e75f7edea97a54f9245b4ab0bddc0114998b47
Heitor Pascoal de Bittencourt <heitorPB@users.noreply.github.com>
2018-05-19 21:46:22 -0300

    bump version to 2.2.5

--- source/conf.py
+++ source/conf.py
@@ -43,7 +43,7 @@ author = 'Grupy-Sanca'
 # built documents.
 #
 # The short X.Y version.
-version = '2.2.4'
+version = '2.2.5'
 # The full version, including alpha/beta/rc tags.
 release = version
 
